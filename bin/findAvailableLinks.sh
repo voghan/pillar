ff312e892cf669644ba674741fc6192e429855f9
Brian Vaughn <brianvaughn@brians-air.lan>
2026-07-08 20:05:14 -0500

    Added GraphQL support, example queries, persisted queries and scripts to execute queries

---
+++ bin/findAvailableLinks.sh
@@ -0,0 +1,2 @@
+# Execute with variables in the URL path
+curl -u admin:admin "http://localhost:4502/graphql/execute.json/pillar/find-available-links;text=Learnmore;url=/content/page/learn"
---
+++ bin/getAllHeroCards.sh
@@ -0,0 +1,2 @@
+# Execute with variables in the URL path
+curl -u admin:admin  http://localhost:4502/graphql/execute.json/pillar/all-hero-cards
